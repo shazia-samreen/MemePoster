4fd0ca02565b38d7937be5440f9bccacd342bd44
shazia-samreen <arshiyanoureen25@gmail.com>
2021-02-17 19:28:08 +0530

    new commit with install.sh  and other .sh files

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-sudo apt update
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# sudo apt update
+# curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+# sudo apt install -y mongodb-org
+# sudo systemctl start mongod
+# sudo systemctl enable mongod
+# mongo memeDB --eval "db.dropDatabase()"
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
+sudo systemctl status --no-pager mongod
 sudo systemctl enable mongod
-mongo memeDB --eval "db.dropDatabase()"
